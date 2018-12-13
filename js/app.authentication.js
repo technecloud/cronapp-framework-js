e950d8381fc0191fe306deab648bf9a0a602bbfe
Henrique Chang <henrique.queiroz@cronapp.io>
2018-12-13 10:43:34 -0300

    customTranslateLoader

--- js/app.authentication.js
+++ js/app.authentication.js
@@ -172,13 +172,14 @@ var app = (function() {
         $translateProvider.useStaticFilesLoader({
           files: [
             {
-              prefix: 'i18n/locale_',
-              suffix: '.json'
-            },
-            {
               prefix: 'plugins/cronapp-framework-js/i18n/locale_',
               suffix: '.json'
             }]
+        });
+
+        $translateProvider.useLoader('customTranslateLoader', {
+          prefix: 'i18n/locale_',
+          suffix: '.json'
         });
 
         $translateProvider.registerAvailableLanguageKeys(
@@ -351,6 +352,78 @@ app.copyContext = function(fromContext, toContext, controllerName) {
     }
   }
 };
+
+app.factory('customTranslateLoader', function ($http, $q) {
+
+  return function (options) {
+
+    if (!options || (!angular.isArray(options.files) && (!angular.isString(options.prefix) || !angular.isString(options.suffix)))) {
+      throw new Error('Couldn\'t load static files, no files and prefix or suffix specified!');
+    }
+
+    if (!options.files) {
+      options.files = [{
+        prefix: options.prefix,
+        suffix: options.suffix
+      }];
+    }
+
+    var load = function (file) {
+      if (!file || (!angular.isString(file.prefix) || !angular.isString(file.suffix))) {
+        throw new Error('Couldn\'t load static file, no prefix or suffix specified!');
+      }
+
+      var deferred = $q.defer();
+
+      $http(angular.extend({
+        url: [
+          file.prefix,
+          options.key,
+          file.suffix
+        ].join(''),
+        method: 'GET',
+        params: ''
+      }, options.$http)).success(function (data) {
+        deferred.resolve(data);
+      }).error(function (data) {
+        deferred.reject(options.key);
+      });
+
+      return deferred.promise;
+    };
+
+    var deferred = $q.defer(),
+        promises = [],
+        length = options.files.length;
+
+    for (var i = 0; i < length; i++) {
+      promises.push(load({
+        prefix: options.files[i].prefix,
+        key: options.key,
+        suffix: options.files[i].suffix
+      }));
+    }
+
+    $q.all(promises).then(function (data) {
+      data = [data[0]];
+      var length = data.length,
+          mergedData = {};
+
+      for (var i = 0; i < length; i++) {
+        for (var key in data[i]) {
+          mergedData[key] = data[i][key];
+        }
+      }
+
+      deferred.resolve(mergedData);
+    }, function (data) {
+      deferred.reject(data);
+    });
+
+    return deferred.promise;
+  };
+
+});
 
 window.safeApply = function(fn) {
   var phase = this.$root.$$phase;
