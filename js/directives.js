ff48c10180ca47041bd999139441c98194cc521b
henriquechang-cronapp <henrique.queiroz@cronapp.io>
2019-09-23 09:30:42 -0300

    [CRONAPP-157] - Corrigindo ==

--- js/directives.js
+++ js/directives.js
@@ -2494,7 +2494,7 @@
                 var item = this.dataItem(this.select());
                 setToActiveInCronappDataSource.bind(this)(item);
                 var cronappDatasource = this.dataSource.transport.options.cronappDatasource;
-                if(options.fieldType && options.fieldType == "key"){
+                if(options.fieldType && options.fieldType === 'key'){
                    cronappDatasource.active = changeObjectField(cronappDatasource, cronappDatasource.active);
                 }
                 if (ngModelCtrl) {
@@ -2503,7 +2503,7 @@
                     this.select().each((i, row)=> {
                       let item = this.dataItem(row);
                       let objInDs = cronappDatasource.findObjInDs(item, false);
-                      if(options.fieldType && options.fieldType == "key"){
+                      if(options.fieldType && options.fieldType === 'key'){
                         objInDs = changeObjectField(cronappDatasource, objInDs);
                       }
                       if (objInDs !== null){
