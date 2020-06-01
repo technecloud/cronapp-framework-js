645cd80400400cff2d5a765d6277b873406f6a08
Thiago Costa <thiagoalvescosta@gmail.com>
2020-06-01 11:50:53 -0300

    CRONAPP-1944 - Problemas ao usar Lista Dinâmica com Parâmetros e Filtros

--- js/directives.js
+++ js/directives.js
@@ -3370,7 +3370,10 @@
                                   var found = _goTo(_scope, _combobox, data);
                                   if (!found) {
                                     _dataSource.data.push(data);
-                                    _goTo(_scope, _combobox, data);
+                                    found = _goTo(_scope, _combobox, data);
+                                  }
+                                  if (found) {
+                                   modelSetter(_scope, found[select.dataValueField]);
                                   }
                                 } else {
                                   modelSetter(_scope, null);
@@ -3472,6 +3475,9 @@
              */
 
             var defineInitialValue = function() {
+              if (combobox.definingInitialValue) {
+                return;
+              }
               if (!combobox.isEvaluating) {
                 var currentValue = combobox.value();
                 var nextValue = null;
@@ -3489,13 +3495,35 @@
 
 
                   if (nextValue == null && select.firstValue) {
-                    if (dataSourceScreen && dataSourceScreen.data.length) {
-                      nextValue = dataSourceScreen.data[0][select.dataValueField];
+                    if (dataSourceScreen) {
+                      combobox.definingInitialValue = true;
+                      dataSourceScreen.fetch({
+                        params: {
+                          $top: 1
+                          }
+                      }, {
+                        success: function(data) {
+                          if (data.length) {
+                            dataSourceScreen.data.push(data[0]);
+                            nextValue = data[0][select.dataValueField];
+                          }
+                          modelSetter(_scope, nextValue);
+                          forceChangeModel(nextValue);
+                          combobox.definingInitialValue = false;
+                        },
+                        error: function() {
+                          combobox.definingInitialValue = false;
+                        },
+                        canceled: function() {
+                          combobox.definingInitialValue = false;
+                        }
+                      }, undefined, {lookup : true});
                     }
+                  } else {
+                    modelSetter(_scope, nextValue);
+                    forceChangeModel(nextValue);
+                    combobox.definingInitialValue = false;
                   }
-
-                  modelSetter(_scope, nextValue);
-                  forceChangeModel(nextValue);
                 }
               }
               else {
@@ -3506,24 +3534,11 @@
             var _ngModelCtrl = ngModelCtrl;
             if (dataSourceScreen != null && dataSourceScreen != undefined) {
               dataSourceScreen.addDataSourceEvents({
-                refresh: function() {
+                overRideRefresh: function() {
+                  dataSourceScreen.fetched = false;
+                  dataSourceScreen.cleanup();
                   defineInitialValue();
-                },
-                read: function(data) {
-
-                  var fromCombo = combobox.options.fromCombo;
-                  combobox.options.fromCombo = false;
-                  if (!fromCombo) {
-                    combobox.options.readData = data;
-                    this.dataSource.read();
-
-
-                    if (combobox.options.lazyFirstValue) {
-                      combobox.options.lazyFirstValue = false;
-                      defineInitialValue();
-                    }
-                  }
-                }.bind(combobox)
+                }
               });
             }
 
@@ -3623,11 +3638,7 @@
                 combobox.options.lazyFirstInitialValue = true;
               }
               else if (select.firstValue) {
-                combobox.options.lazyFirstValue = true;
-
-                if (dataSourceScreen.lazy && !dataSourceScreen.fetched) {
-                  dataSourceScreen.fetch({});
-                }
+                defineInitialValue();
               }
 
             }
@@ -5484,6 +5495,9 @@ app.kendoHelper = {
       cronappDatasource.fetch(fetchData, {
             success:  function(data) {
               if (e.success) {
+                if (self.options.combobox) {
+                  self.options.combobox.options.expanded = true;
+                }
                 e.success(data);
                 if (self.options && self.options.combobox && self.options.combobox.element[0].id) {
                   var expToFind = " .k-animation-container";
