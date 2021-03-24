0f5d8913b9d0a04359d0187ed3e7441d1f3373f6
tiagoromano <tiago.romano@gmail.com>
2021-03-24 15:54:35 -0300

    CRONAPP-3387  Tela branca é exibida é ao rodar projeto sem backend (#413)
    * CRONAPP-3387 Tela branca é exibida é ao rodar projeto sem backend
    
    * CRONAPP-3387 Tela branca é exibida é ao rodar projeto sem backend

---
+++ js/stateProvider.js
@@ -0,0 +1,146 @@
+window.stateProviderDefine = {
+  handle: (s) => {
+    if (window.customStateProvider) {
+      window.customStateProvider(s);
+    }
+    else {
+      let type = $('script[src*=app\\.js]')[0].src.split("type=")[1];
+      !window.stateProviderDefine[type] || window.stateProviderDefine[type](s);
+    }
+  },
+  withAuth: (s) => {
+    s
+    .state('login', {
+      url: "",
+      controller: 'LoginController',
+      templateUrl: 'views/login.view.html'
+    })
+
+    .state('reset-password', {
+      url: "/public/reset-password",
+      controller: 'ResetPasswordController',
+      templateUrl: 'views/public/reset-password.view.html'
+    })
+
+    .state('social', {
+      url: "/connected",
+      controller: 'SocialController',
+      templateUrl: 'views/login.view.html'
+    })
+
+    .state('socialError', {
+      url: "/notconnected",
+      controller: 'SocialController',
+      templateUrl: 'views/login.view.html'
+    })
+
+    .state('main', {
+      url: "/",
+      controller: 'LoginController',
+      templateUrl: 'views/login.view.html'
+    })
+
+    .state('publicRoot', {
+      url: "/public/{name:.*}",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/public/' + urlattr.name + '.view.html';
+      }
+    })
+
+    .state('public', {
+      url: "/home/public",
+      controller: 'PublicController',
+      templateUrl: function(urlattr) {
+        return 'views/public/home.view.html';
+      }
+    })
+
+    .state('public.pages', {
+      url: "/{name:.*}",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/public/' + urlattr.name + '.view.html';
+      }
+    })
+
+    .state('home', {
+      url: "/home",
+      controller: 'HomeController',
+      templateUrl: 'views/logged/home.view.html',
+      resolve: {
+        data: function ($translate) {
+          $translate.refresh();
+        }
+      }
+    })
+
+    .state('home.pages', {
+      url: "/{name:.*}",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/' + urlattr.name + '.view.html';
+      }
+    })
+
+    .state('404', {
+      url: "/error/404",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/error/404.view.html';
+      }
+    })
+
+    .state('403', {
+      url: "/error/403",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/error/403.view.html';
+      }
+    });
+  },
+  withoutAuth: (s) => {
+    s
+    .state('index', {
+      url: "",
+      controller: 'HomeController',
+      templateUrl: 'views/home.view.html'
+    })
+
+    .state('main', {
+      url: "/",
+      controller: 'HomeController',
+      templateUrl: 'views/home.view.html'
+    })
+
+    .state('home', {
+      url: "/home",
+      controller: 'HomeController',
+      templateUrl: 'views/home.view.html'
+    })
+
+    .state('home.pages', {
+      url: "/{name:.*}",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/' + urlattr.name + '.view.html';
+      }
+    })
+
+    .state('404', {
+      url: "/error/404",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/error/404.view.html';
+      }
+    })
+
+    .state('403', {
+      url: "/error/403",
+      controller: 'PageController',
+      templateUrl: function(urlattr) {
+        return 'views/error/403.view.html';
+      }
+    });
+  }
+};
