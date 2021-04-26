e69330f33277a24d3f0d1d805b0a88ee54e3f433
lincolnminto-cronapp <36731613+lincolnminto-cronapp@users.noreply.github.com>
2021-04-26 10:17:06 -0300

    CRONAPP-3657 - Projeto com SSO não consegue executar (#459)
    Solução
    Obter informações do redirect SSO e recarregar página

--- js/app.js
+++ js/app.js
@@ -29,6 +29,18 @@ var onloadCallback = function() {
   window.grecaptcha.reset();
 };
 
+// OAuth 2 / SSO Handler:
+window.onmessage = function (e) {
+  if(e.data && e.data.type === 'sso_user'){
+    // close sso window
+    if(window.ssoWindow) {
+      window.ssoWindow.close();
+    }
+    // reload mobileapp
+    window.location.reload();
+  }
+};
+
 var app = (function() {
 
   return angular.module('MyApp', cronappModules)
