3aa2cd7bb36c85179b389537edb8f44cca264f0a
Jaqueline Peixer <jaqueline.peixer@cronapp.io>
2021-04-28 15:39:30 -0300

    ajustes finais

--- js/directives.js
+++ js/directives.js
@@ -76,7 +76,7 @@
     return {
        replace: true,
        restrict: "E" ,  
-       link: async function (scope, element, attrs, ngModelCtrl) {
+       link: async function (scope, attrs) {
           scope.attrs = attrs;
        }
      }
