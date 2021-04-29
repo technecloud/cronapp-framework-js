ab648c1397231260fe3f6dde3f47dcc4775dbf27
Lincoln Minto <lincoln.minto@cronapp.io>
2021-04-29 16:34:58 -0300

    CRONAPP-3712 - Converter bloco Login SSO Oauth para uma async call e poder realizar ações após o sucesso de login.
    Solução
    Utilizar o bloco de Logar via SSO

--- js/controllers.js
+++ js/controllers.js
@@ -97,7 +97,7 @@
       }
     }
     $scope.redirectToLogin = function() {
-      $window.location.href = '/login';
+      $scope.cronapi.social.ssoLogin();
     };
     $scope.autoLogin = function(){
       if(localStorage.getItem('_u') && JSON.parse(localStorage.getItem('_u')).token ){
