536ed5dc25a0fb53429da2cc2cb30fc60031b598
Jaqueline Peixer <jaqueline.peixer@cronapp.io>
2021-06-16 11:20:23 -0300

    ajustes no component crn-image-menu

--- js/directives.js
+++ js/directives.js
@@ -517,19 +517,6 @@
           }
         }
       }])
-
-      .directive('onErrorSrc', function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.bind('error', function () {
-                    if (attrs.src != attrs.onErrorSrc) {
-                        attrs.$set('src', attrs.onErrorSrc);
-                    }
-                });
-            }
-        }
-      })
-
       .directive('ngClick', [function() {
         'use strict';
         return {
