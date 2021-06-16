234fd323903c51268349056517e146dc58e5d52b
Jaqueline Peixer <jaqueline.peixer@cronapp.io>
2021-06-16 10:40:26 -0300

    on-error-src

--- js/directives.js
+++ js/directives.js
@@ -517,6 +517,19 @@
           }
         }
       }])
+
+      .directive('onErrorSrc', function () {
+        return {
+            link: function (scope, element, attrs) {
+                element.bind('error', function () {
+                    if (attrs.src != attrs.onErrorSrc) {
+                        attrs.$set('src', attrs.onErrorSrc);
+                    }
+                });
+            }
+        }
+      })
+
       .directive('ngClick', [function() {
         'use strict';
         return {
